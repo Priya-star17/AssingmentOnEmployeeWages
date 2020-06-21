80bcc79a90af6892a3bd2c434f2fa6c36c634295
Priya <priyadalai1393@gmail.com>
2020-06-22 01:12:14 +0530

    Employee Attendence

---
+++ UC1.java
@@ -0,0 +1,14 @@
+package com.ass23;
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		
+				int check=(int)Math.floor(Math.random()*10)%2; 
+		 		if(check==1) 
+		 			System.out.println("Employee is Present"); 
+		 		else 
+		 			System.out.println("Employee is Absent");  
+	}
+
+}
