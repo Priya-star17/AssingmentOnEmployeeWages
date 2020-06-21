28fd2231886c928be010b6c4b4364ae88d7bb7d8
Priya <priyadalai1393@gmail.com>
2020-06-22 01:36:38 +0530

    Employee Wages for each company

---
+++ UC9.java
@@ -0,0 +1,20 @@
+package com.ass23;
+
+
+	public class UC9
+	 { 
+	 	public static void main(String[] args) 
+	 	{ 
+	 
+	 
+	 		System.out.println("\n"); 
+	 		EmployeeWageUC9 obj1=new EmployeeWageUC9("BSNL",120,20,100); 
+	 		obj1.calculateEmpWage(); 
+	 		System.out.println("\n"); 
+			EmployeeWageUC9 obj2=new EmployeeWageUC9("Jio",100,25,50); 
+	 		obj2.calculateEmpWage(); 
+	 	} 
+ 
+
+
+}
